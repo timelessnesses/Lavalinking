2298f5bd03269cc3e84371c45d78dfe87204d8e8
Code beautifier and Code linter <timelessnesses@users.noreply.github.com>
2022-07-15 13:37:09 +0000

    beautified and linted code you stupid nerd

--- src/event.py
+++ src/event.py
@@ -1,8 +1,9 @@
-import traceback
-import discord
-from discord.ext import commands
 import difflib
 import sys
+import traceback
+
+import discord
+from discord.ext import commands
 
 
 class Events(commands.Cog):
